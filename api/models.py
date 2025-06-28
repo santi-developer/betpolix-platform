d85fbcf0f47fec67256bb92ee8bb33c05614ec48
santiago <ivsasafo30@hotmail.com>
2025-06-27 23:50:20 -0500

    Agregar campo saldo al modelo usuario

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Usuario(AbstractUser):
+    saldo = models.DecimalField(
+        max_digits=10,  # hasta 99999999.99
+        decimal_places=2,
+        default=0.00,
+        verbose_name='Saldo disponible'
+    )
+
+    def __str__(self):
+        return f"{self.username} - Saldo: ${self.saldo}"
